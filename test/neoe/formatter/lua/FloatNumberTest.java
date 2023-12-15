b61e805660164947e11f09c7671379d87d570da2
rusefillc <sdfsdfqsf2334234234>
2023-12-15 12:14:51 -0500

    junit 5

--- test/neoe/formatter/lua/FloatNumberTest.java
+++ test/neoe/formatter/lua/FloatNumberTest.java
@@ -1,8 +1,8 @@
 package neoe.formatter.lua;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FloatNumberTest {
     @Test
